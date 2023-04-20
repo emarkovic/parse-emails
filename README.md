3eb8f8bf6840e98d17cc5c02bec6437b5608146f
Ena <enamarkovic27@gmail.com>
2023-04-20 12:53:02 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Install dependencies
+This function uses an external library caled mailparser -> https://github.com/nodemailer/mailparser
+
+For this to function you must install that dependency by doing:
+
+`$ npm install`
+
+You only need to do this after cloning the repo the first time.
+
+Please ensure you only run email-parser.js from this director - moving the file around will result in it losing reference to mailparser dependency.
+
+# How to run
+After you have installed dependencies, run 
+
+`$ node email-parser.js path/to/emls`
+
+This will generate a text file for you. 
+
+For more help and aditional options and examples run:
+
+`$ node email-parser.js -h` 
---
+++ email-parser.js
@@ -0,0 +1,103 @@
+'use strict';
+
+const fs = require('fs');
+const simpleParser = require('mailparser').simpleParser;
+
+
+// HELP text
+if (process.argv[2]) {
+    const firstArg = process.argv[2].toLowerCase();
+    if (firstArg === "-h" || firstArg == "--h") {
+        console.log("USAGE");
+        console.log("$ node email-parser.js <PATH TO FOLDER WITH EML FILES> <OUTPUT FILE NAME>")
+        console.log("- PATH TO FOLDER WITH EML FILES - folder where the contents are all eml files")
+        console.log("- OUTPUT FILE NAME - optional - desired name of output file without file extension")
+    
+        console.log()
+            
+        console.log("OUTPUT")
+        console.log("Program will generate a text file. File name will default to the name of the root folder provided.")
+    
+        console.log()
+    
+        console.log("EXAMPLES");
+        console.log("$ node email-parser.js path/to/emls")
+        console.log("    - this generates emls.txt in whatever folder the command was run in")
+        console.log()
+        console.log("$ node email-parser.js path/to/emls example")
+        console.log("    - this generates example.txt in whatever folder the command was run in")
+
+        process.exit()
+    }
+}
+
+// argv[0] is node executable path
+// argv[1] is path to this file
+const pathToFolder = process.argv[2];
+verifyArgs()
+const outputFileName = (process.argv[3]|| processPath()) + ".txt";
+
+work();
+
+////////////////////////////////////////////////////////// FUNCTIONS //////////////////////////////////////////////////////////
+
+function verifyArgs() {
+    // pathToFolder
+    if (pathToFolder === undefined || pathToFolder === null || pathToFolder.trim() === "") {
+        console.error("Error: path to folder is required.")
+        process.exit()
+    }
+
+    // check that its a folder
+    if (processPath().includes(".")) {
+        console.error("Error: please specify a path to a folder containing eml files.")
+    }
+
+    let pathExists = fs.existsSync(pathToFolder)
+    if (!pathExists) {
+        console.error("Error: specified path does not exist.")
+        process.exit()
+    }
+}
+
+function processPath() {
+    const pathArr = pathToFolder.split("/");
+    return pathArr[pathArr.length - 1];
+}
+
+function work() {
+    const emlFiles = fs.readdirSync(pathToFolder).filter(file => file.includes(".eml"));
+    console.log(emlFiles)
+
+    const emlPromises = emlFiles.map(file => {
+        const fileBuffer = fs.readFileSync(pathToFolder + "/" + file)
+        return simpleParser(fileBuffer).then(parsed => {
+            return file.replace(".eml", "") + "|" + (parsed.text || "").trim()
+        })
+    });
+
+    Promise.all(emlPromises).then(values => {
+        const outputStr = values.join("\n");
+        fs.writeFileSync(outputFileName, outputStr)
+    })
+
+    // fs.readdir(pathToFolder, async function (err, files) {
+    //     //handling error
+    //     if (err) {
+    //         return console.error('Error: Unable to scan directory: ' + err);
+    //     } 
+
+    //     const emlFiles = files.filter(file => file.includes(".eml"))
+    //     console.log(emlFiles)
+
+    //     const buffer = Buffer.from("");
+
+    //     emlFiles.forEach(async file => {
+    //         const fileBuffer = fs.readFileSync(pathToFolder + "/" + file)
+    //         const parsedEmail = await simpleParser(fileBuffer);
+    //         buffer.write(`${file}|${parsedEmail}\n`)
+    //     })
+
+    //     console.log(buffer.toString())
+    // });
+}
